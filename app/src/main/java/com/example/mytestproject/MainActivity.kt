895a795bd75adddcc762515df4073212f62f3385
Artem Polovin <polovinartem@gmail.com>
2020-05-26 20:19:13 -0700

    [task-6] create NavigationDrawer with three Fragments

--- app/src/main/java/com/example/mytestproject/MainActivity.kt
+++ app/src/main/java/com/example/mytestproject/MainActivity.kt
@@ -2,11 +2,48 @@ package com.example.mytestproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
+import androidx.fragment.app.Fragment
+import com.google.android.material.navigation.NavigationView
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+    lateinit var fragment: Fragment
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        nav_view.setNavigationItemSelectedListener(this)
+        setSupportActionBar(toolbar)
+        val toggle = ActionBarDrawerToggle(
+            this, drawer_layout, toolbar,
+            R.string.navigation_drawer_open, R.string.navigation_drawer_close
+        )
+        drawer_layout.addDrawerListener(toggle)
+        toggle.syncState()
+
     }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.nav_first_fragment -> fragment = FragmentFirst()
+            R.id.nav_second_fragment -> fragment = FragmentSecond()
+            R.id.nav_third_fragment -> fragment = FragmentThird()
+        }
+        supportFragmentManager.beginTransaction().replace(R.id.fragment_container, fragment).commit()
+        item.isChecked = true
+        title = item.title
+        drawer_layout.closeDrawers()
+        return true
+    }
+
+    override fun onBackPressed() {
+        if (drawer_layout.isDrawerOpen(GravityCompat.START)) {
+            drawer_layout.closeDrawer(GravityCompat.START)
+        } else super.onBackPressed()
+    }
+
 }
