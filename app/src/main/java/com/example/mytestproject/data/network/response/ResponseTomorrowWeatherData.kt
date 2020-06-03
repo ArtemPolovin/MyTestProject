a465101a879fd1f9f21093de5b6babe4c5d4d5d6
Artem Polovin <polovinartem@gmail.com>
2020-06-03 10:35:00 -0700

    [task-9] removed all unnecessary fields

--- app/src/main/java/com/example/mytestproject/data/network/response/ResponseTomorrowWeatherData.kt
+++ app/src/main/java/com/example/mytestproject/data/network/response/ResponseTomorrowWeatherData.kt
@@ -1,11 +1,3 @@
 package com.example.mytestproject.data.network.response
 
-data class ResponseTomorrowWeatherData(
-    val city_name: String,
-    val country_code: String,
-    val data: List<Data>,
-    val lat: String,
-    val lon: String,
-    val state_code: String,
-    val timezone: String
-)
+data class ResponseTomorrowWeatherData(val city_name: String, val data: List<Data>)
--- app/src/main/java/com/example/mytestproject/data/network/response/Weather.kt
+++ app/src/main/java/com/example/mytestproject/data/network/response/Weather.kt
@@ -1,7 +1,6 @@
 package com.example.mytestproject.data.network.response
 
 data class Weather(
-    val code: Int,
     val description: String,
     val icon: String
 )
