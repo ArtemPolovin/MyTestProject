360d379093b629cdabb66359de82bf4c1ec82a49
Artem Polovin <polovinartem@gmail.com>
2020-09-17 18:35:41 -0700

    [task-18] created separate class for all worker logic

--- app/src/main/java/com/example/mytestproject/App.kt
+++ app/src/main/java/com/example/mytestproject/App.kt
@@ -1,18 +1,13 @@
 package com.example.mytestproject
 
 import android.app.Application
-import android.util.Log
 import androidx.work.Configuration
-import androidx.work.ExistingPeriodicWorkPolicy
-import androidx.work.PeriodicWorkRequestBuilder
 import androidx.work.WorkManager
 import com.example.data.di.WeatherDataModule
-import com.example.mytestproject.di.CityFilterModule
 import com.example.mytestproject.di.DaggerWeatherDataComponent
 import com.example.mytestproject.di.WeatherDataComponent
-import com.example.mytestproject.workManager.DeleteOldWeatherDataWorker
 import com.example.mytestproject.workManager.SampleWorkerFactory
-import java.util.concurrent.TimeUnit
+import com.example.mytestproject.workManager.WorkScheduler
 import javax.inject.Inject
 
 class App : Application() {
@@ -21,6 +16,8 @@ class App : Application() {
 
     @Inject
     lateinit var sampleWorkerFactory: SampleWorkerFactory
+    @Inject
+    lateinit var workScheduler: WorkScheduler
 
     override fun onCreate() {
         super.onCreate()
@@ -35,25 +32,7 @@ class App : Application() {
             Configuration.Builder().setWorkerFactory(sampleWorkerFactory).build()
         )
 
-        scheduleSyncWorker()
-    }
-
-    private fun scheduleSyncWorker() {
-        val workManager by lazy { WorkManager.getInstance(this) }
-        val workRequest =
-            PeriodicWorkRequestBuilder<DeleteOldWeatherDataWorker>(24, TimeUnit.HOURS)
-                .addTag("cancel")
-                .build()
-
-        workManager.apply {
-            enqueueUniquePeriodicWork(
-                "delete old weather data",
-                ExistingPeriodicWorkPolicy.KEEP,
-                workRequest
-            )
-        }
-     //   WorkManager.getInstance(this).cancelAllWork()
-
+        workScheduler.scheduleSyncWorker(this)
     }
 
 
