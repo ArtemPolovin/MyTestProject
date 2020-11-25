2bdad20121c9e28206d25611d96b116a89fea004
Artem Polovin <polovinartem@gmail.com>
2020-11-25 12:30:08 -0800

    [task-15] hid search city icon in FragmentSecond and FragmentThird

--- app/src/main/java/com/example/mytestproject/ui/FragmentThird.kt
+++ app/src/main/java/com/example/mytestproject/ui/FragmentThird.kt
@@ -3,6 +3,7 @@ package com.example.mytestproject.ui
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.Menu
 import android.view.View
 import android.view.ViewGroup
 import com.example.mytestproject.R
@@ -15,6 +16,16 @@ class FragmentThird : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_third, container, false)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
+    override fun onPrepareOptionsMenu(menu: Menu) {
+        val menuItem = menu.findItem(R.id.search_city)
+        if(menuItem != null) menuItem.isVisible = false
     }
 
 }
