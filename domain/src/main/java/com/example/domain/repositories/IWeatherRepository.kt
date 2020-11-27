f857b74ffe2198acd55b6c354937c8f90eaf9355
Artem Polovin <polovinartem@gmail.com>
2020-11-27 14:48:19 -0800

    [task-22] made temperature type as celsius as default

--- domain/src/main/java/com/example/domain/repositories/IWeatherRepository.kt
+++ domain/src/main/java/com/example/domain/repositories/IWeatherRepository.kt
@@ -4,7 +4,7 @@ import com.example.domain.models.WeatherData
 import io.reactivex.Single
 
 interface IWeatherRepository {
-    fun getWeatherData(cityId: Int, degreeType: String): Single<WeatherData>
-    fun getDailyWeather(cityId: Int, days: Int, degreeType: String): Single<List<WeatherData>>
+    fun getWeatherData(cityId: Int): Single<WeatherData>
+    fun getDailyWeather(cityId: Int, days: Int): Single<List<WeatherData>>
     fun deleteOldWeatherDataFromEntity()
 }
